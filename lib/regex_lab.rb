9cea7b895b8277ece4d19c900be8c653d416c918
Brandon Green <brandon@brandonleeproductions.com>
2018-05-18 02:50:42 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,25 +1,19 @@
 def starts_with_a_vowel?(word)
-  if word = %w{ afoot Excellent incredible Absolute unreal Inconceivable octopus }
-    return true
-  elsif word = %w{ chandoo Crenel crooked Flimsy folksy Dog Hermit
-   makeshift Palazzi sixfold Truth }.match
-    return false
-
-  end
+  word.match(/^[aeiouAEIOU]/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w+ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match(/^[A-Z].+[.?!"]$/) ? true : false
 end
 
 def valid_phone_number?(phone)
-
+  phone.match(/\(?\d{3}[) ]?\d{3}[- ]?\d{4}$/) ? true : false
 end
